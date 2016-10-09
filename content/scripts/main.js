1f90c093b39d01f7aa0ca03c7b518e4a10797c37
Martin Georgiev <m.georgievv@gmail.com>
2016-10-10 02:23:55 +0300

    changing the field size

--- content/scripts/main.js
+++ content/scripts/main.js
@@ -1,43 +1,43 @@
 // Create the canvas
-var canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
-canvas.width = 512;
-canvas.height = 480;
+let canvas = document.createElement("canvas");
+let ctx = canvas.getContext("2d");
+canvas.width = 600;
+canvas.height = 600;
 document.body.appendChild(canvas);
 
 // Background image
-var bgReady = false;
-var bgImage = new Image();
+let bgReady = false;
+let bgImage = new Image();
 bgImage.onload = function () {
     bgReady = true;
 };
 bgImage.src = "images/background1.png";
 
 // Hero image
-var heroReady = false;
-var heroImage = new Image();
+let heroReady = false;
+let heroImage = new Image();
 heroImage.onload = function () {
     heroReady = true;
 };
 heroImage.src = "images/hero1.png";
 
 // Monster image
-var monsterReady = false;
-var monsterImage = new Image();
+let monsterReady = false;
+let monsterImage = new Image();
 monsterImage.onload = function () {
     monsterReady = true;
 };
 monsterImage.src = "images/monster1.png";
 
 // Game objects
-var hero = {
+let hero = {
     speed: 256 // movement in pixels per second
 };
-var monster = {};
-var monstersCaught = 0;
+let monster = {};
+let monstersCaught = 0;
 
 // Handle keyboard controls
-var keysDown = {};
+let keysDown = {};
 
 addEventListener("keydown", function (e) {
     keysDown[e.keyCode] = true;
@@ -48,7 +48,7 @@ addEventListener("keyup", function (e) {
 }, false);
 
 // Reset the game when the player catches a monster
-var reset = function () {
+let reset = function () {
     hero.x = canvas.width / 2;
     hero.y = canvas.height / 2;
 
@@ -58,7 +58,7 @@ var reset = function () {
 };
 
 // Update game objects
-var update = function (modifier) {
+let update = function (modifier) {
     if (38 in keysDown) { // Player holding up
         hero.y -= hero.speed * modifier;
     }
@@ -85,7 +85,7 @@ var update = function (modifier) {
 };
 
 // Draw everything
-var render = function () {
+let render = function () {
     if (bgReady) {
         ctx.drawImage(bgImage, 0, 0);
     }
@@ -107,9 +107,9 @@ var render = function () {
 };
 
 // The main game loop
-var main = function () {
-    var now = Date.now();
-    var delta = now - then;
+let main = function () {
+    let now = Date.now();
+    let delta = now - then;
 
     update(delta / 1000);
     render();
@@ -121,10 +121,10 @@ var main = function () {
 };
 
 // Cross-browser support for requestAnimationFrame
-var w = window;
+let w = window;
 requestAnimationFrame = w.requestAnimationFrame || w.webkitRequestAnimationFrame || w.msRequestAnimationFrame || w.mozRequestAnimationFrame;
 
 // Let's play this game!
-var then = Date.now();
+let then = Date.now();
 reset();
 main();
